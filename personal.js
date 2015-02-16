a077b4d3b816653b0df06cac1bf78be231857db2
Elizabeth Dutton <emddutton@gmail.com>
2015-02-16 12:21:06 -0700

    little changes

--- personal.js
+++ personal.js
@@ -5,8 +5,21 @@ $(function() {
     $('.menu').on("click", function(){
         $('.menu').removeClass('active');
         $('.content').removeClass('active');
-
         $(this).addClass("active");
+
+    if(this.id == "aboutmenu"){
+        $(".fade").hide();
+        $("#about").show();
+    }
+    else if(this.id == "portmenu"){
+        $(".fade").hide();
+        $("#portfolio").show();
+    }
+        else if(this.id == "resumemenu"){
+        $(".fade").hide();
+        $("#resume").show();
+    }
+
 
 
 
